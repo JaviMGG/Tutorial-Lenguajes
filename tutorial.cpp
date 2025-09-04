2b356f4ac5c6330d091442b36fdc851a2468bb61
JaviMGG <javimm1012@gmail.com>
2025-09-04 19:09:18 +0200

    C++ (sin terminar)

--- tutorial.cpp
+++ tutorial.cpp
@@ -12,6 +12,28 @@ lineas
 
 
 
+//############################################ EXTRACCION E INSERCION DE DATOS (PRINT Y SCANNER) ##################################################
+
+/**
+ * Dos tipos de operadores:
+ * cout << ... (extraccion) /un print
+ * cin  >> ... (insercion)  /un scanner
+*/
+
+void funcionDeInsercionYExtraccion(){
+	using namespace std;
+	string nombre, apellido;
+	cout << "como te llamas" <<endl;
+	cin >> nombre;	//si no deja escribir el nombre, HAY QUE IR A: configuracion -> code runner ->"run in terminal" y activarlo
+	cout << "te llamas " << nombre << endl;
+
+	cout << " y que apellido tienes\n";
+	getline(cin>>ws, apellido);	//esta funcion lo que hace es que coge el string que se escriba, que escribe/recibe desde el primer parametro (el ws es para evitar fallos) y lo guarda en el segundo
+
+}
+
+
+
 //############################################ TIPO DE DATOS Y CONVERSION DE TIPOS ##################################################
 
 void funcionDeTipos(){
@@ -34,15 +56,16 @@ char c = 'c';
 //TEXTOS (string)
 std::string s = "Hola";
 
-/*
-SI EXISTIERA PREVIAMENTE EL "using namespace std"
-habria que poner:
-string s = "Hola";
+/** 
+ * SI EXISTIERA PREVIAMENTE EL "using namespace std"
+ * habria que poner:
+ * string s = "Hola";
 */
 
 //DATOS CONSTANTES (const)
 const double pi = 3.14;
 /*al poner el "const" delante, SOLAMENTE ES DE LECTURA, no se puede editar*/
+
 
 //Conversion de tipos
 
@@ -57,7 +80,6 @@ int caracter = 100;
 std::cout << "valor de la variable caracter: " << caracter << std::endl;
 char caracterReal = (char) caracter;
 std::cout << "valor de la variable caracter EN LA TABLA ASCII (el valor que le corresponde): " << caracterReal << std::endl;
-
 
 
 
@@ -77,6 +99,7 @@ namespace primero{
 namespace segundo{
 	int x = 2;
 }
+
 /*
  el más "famoso" es std, 
  si ponemos using namespace std, 
@@ -128,7 +151,8 @@ using Entero = int; //Equivale a un typedef
 
 
 
-//############################################ OPERADORES ARITMETICOS ##################################################
+//############################################ OPERADORES ##################################################
+
 /**
  * Tipos de operadores aritméticos:
  * 1- Suma
@@ -142,8 +166,15 @@ using Entero = int; //Equivale a un typedef
  * 2- LAS MULTIPLICACIONES * Y DIVISIONES /
  * 3. LAS SUMAS + Y LAS RESTAS -
  * 
+ * 
+ * Tipos de operadores lógicos:
+ * 1- and	(&&)
+ * 2- or	(||)
+ * 3- not	(!)
 */
 
+
+//Operadores aritméticos
 
 //Suma
 void pruebaAritmeticosSUMA(){
@@ -200,8 +231,113 @@ void pruebaAritmeticosMODULO(){
 }
 
 
+//Operadores Lógicos
 
-//############################################ ENTRADA/SALIDA DE DATOS ##################################################
+//and
+bool pruebaAND(){
+	return 2 < 3 and 8 > 23;	//devuelve true si 2 < 3 Y SI 8 > 23, si no se cumple alguna de las 2, devuelve false
+}
+
+//or
+bool pruebaOR(){
+	return 2 < 3 and 8 > 23;	//devuelve true si 2 < 3 O SI 8 > 23, si se cumple alguna de las 2, devuelve true
+}
+
+//not
+bool pruebaNOT(){
+	bool verdadero = true;
+	return !verdadero;			//devuelve false porque la not invierte el valor
+}
+
+
+
+//############################################ FUNCIONES ÚTILES EN C++ ##################################################
+
+/**
+ * max(x, y)		=> devuelve el máximo de los parametros x e y 	=> x e y puede ser valores o variables
+ * min(x, y)		=> devuelve el minimo de los parametros x e y	=> x e y puede ser valores o variables
+ * 
+ * libreria cmath	=> #include <cmath>
+ * algunos de sus metodos:
+ * 
+ * pow(x, y)		=> eleva el parametro x a y (potencia)
+ * sqrt(x)			=> calcula la raiz cuadrada de x
+ * abs(x)			=> valor absoluto de x
+ * round(x)			=> aproxima x (hace ponderacion/redondea)
+ * ceil(x)			=> redondear x hacia arriba
+ * floor(x)			=> redondear x hacia abajo
+*/
+
+void funcionesVarias(){
+	#include <cmath>
+	double x = 2.6;
+	double y = 3;
+	double z;
+
+	z = std::max(x,y);
+	z = std::min(x,y);
+	z = pow(x,y);
+	z = sqrt(x);
+	z = abs(x);
+	z = round(3.65);
+}
+
+
+
+//############################################ CONDICIONALES  ##################################################
+/**
+Tres tipos de condicionales:
+ * ------------------------------------------------------------------------
+ * 1- if, else if, else     => if(condicion){cosa a ejecutar} else if(condicion "secundaria"){cosa a ejecutar} else{cosa a ejecutar si no se cumple las condiciones}
+ * 2- switch                => switch(variable que determina los casos){case X: cosa a ejecutar si variable vale X...}
+ * 3- operadores ternarios  => (comparacion) ? (si se cumple la comparacion) : (si no se cumple la comparacion)
+*/
+
+void funcionCondicionales(){
+	using namespace std;
+	int edad;
+	cout << "Dime tu edad"<< endl;
+	cin >> edad;
+
+	//if, else if, else
+	if(edad < 18){
+		cout << "Eres menor de edad" << endl;
+	}
+	else if(edad > 18){
+		cout << "Eres mayor de edad" << endl;
+	}
+	else{
+		cout << "Tienes " << edad << " años" << endl;
+	}
+
+	//switch
+	cout << "dime tu mes de nacimiento" << endl;
+	int mes;
+	cin >> mes;
+	switch(mes){
+		case 1:
+			cout << "Enero" << endl;
+		break;	//NECESARIO PARA SALIR DEL CASE
+		case 2:
+			cout << "Febrero" << endl;
+		break;
+		default:
+			cout << "Otro mes" << endl;
+				//NO HACE FALTA EL CASE PORQUE ES EL CASO POR DEFECTO
+	}
+
+	//Operador ternario
+	cout << "Escribe de nuevo tu edad, que se me ha olvidado" << endl;
+	int edad; 
+	cin >> edad;
+	string VerificarEdad = edad >= 18 ? "Eres mayor de edad" : "Eres menor de edad";
+	cout << VerificarEdad;
+
+}
+
+
+
+//############################################ FUNCION MAIN y LLAMADA A METODOS ##################################################
 
 int main() {
 	int x; //declaracion
@@ -214,7 +350,8 @@ int main() {
 	pruebaAritmeticosMULTIPLICAR();
 	pruebaAritmeticosDIVIDIR();
 	pruebaAritmeticosMODULO();
-
+	
+	
 	return 0; //al ser una funcion int, tiene que devolver un entero, las funciones VOID NO NECESITAN EL RETURN
 }
 
