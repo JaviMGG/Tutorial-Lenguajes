c5c40ff30dfc9dcac640b3b962b2f818f1440f1c
JaviMGG <javimm1012@gmail.com>
2026-02-05 19:21:18 +0100

    C (sin terminar)

--- tutorial.c
+++ tutorial.c
@@ -18,6 +18,8 @@
 // #include <omp.h>   //Libreria para programación paralela con OpenMP
 #include <stdbool.h>  //Libreria para tipo de dato booleano
 
+
+
 // ################################# Tipos de datos ################################################
 // estructura: <tipo de dato> nombre = <valor(opcional)>;
 
@@ -32,6 +34,91 @@ bool booleano = 1;      // Booleano (0 o 1) // Requiere #include <stdbool.h>
 // Cadenas de caracteres
 char cadena[] = "Hola, Mundo!"; // Cadena de caracteres
 int valores[];
+double matriz[3][3];        // Matriz de 3x3
+
+// Valores constantes
+const float PI = 3.14159;   // Constante de tipo flotante
+const int MAX_VALOR = 100;  // Constante de tipo entero
+#define MAX_TAMANO 50       // Constante definida con #define
+
+
+
+// ################################# Valores static ################################################
+// lo que tenga "static" previamente, UNICAMENTE puede ser accedido desde este archivo, NO desde cualquier parte
+
+static void funcionConStatic()
+{
+    static int variable = 0;
+}
+
+
+
+// ################################# Mostrar por pantalla valores ##################################
+int entero = 0;
+float flotante = 0.0f;
+double doble = 0.0;
+char caracter = ' ';
+char cadena[100] = ""; // Cadena de caracteres con espacio para 100 caracteres
+bool booleano = false; // Valor booleano inicializado a false
+
+void mostrarValores() 
+{
+    printf("Valor del entero: %d\n", entero);
+    printf("Valor del flotante: %.2f\n", flotante);
+    printf("Valor del doble: %.2lf\n", doble);
+    printf("Valor del caracter: %c\n", caracter);
+    printf("Valor de la cadena: %s\n", cadena);
+    printf("Valor del booleano: %d\n", booleano); // Imprime 0 o 1 dependiendo del valor booleano
+}
+
+
+
+// ################################# Bucles ########################################################
+/**
+ * Estructura del bucle for:
+ * for (inicialización; condición; incremento) {
+ *     // Código a ejecutar en cada iteración
+ * }
+ * 
+ * 
+ * Estructura del bucle while:
+ * while (condición) {
+ *     // Código a ejecutar mientras la condición sea verdadera
+ * }
+ * 
+ * 
+ * Estructura del bucle do-while:
+ * do {
+ *     // Código a ejecutar al menos una vez
+ * } while (condición);
+*/
+
+void mostrarBucles()
+{
+    int count = 10;
+    for (int i = 0; i < count; i++)
+    {
+        printf("Valor de i: %d\n", i);
+    }
+    
+    int i = 0;
+    while (i < count)
+    {
+        printf("Valor de i: %d\n", i);
+        i++;
+    }
+    
+
+    int i = 0;
+    do
+    {
+        printf("Valor de i: %d\n", i);
+        i++;
+    } while (i < count);
+    
+}
+
+
 
 // ################################# Funciones #####################################################
 // estructura: <tipo de dato> nombre (parametros) { ... }
@@ -47,6 +134,8 @@ int funcionEjemploSuma(int x, int y) //al ser una funcion de tipo distinto a voi
     int z = x + y;
     return z; //Si o si tiene que devolver un entero (o una funcion que devuelva un entero)
 }
+
+
 
 // ################################# Funcion Main ##################################################
 // estructura: int main() { ... }
